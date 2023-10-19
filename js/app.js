583262248d96319643a1a990d8d9bf8a5db9a9db
LuisMendozaSanchez <tavo.tpc@gmail.com>
2023-10-19 15:36:11 -0600

    primera version proyecto emails

---
+++ js/app.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function(){
+    // Seleccionar los elementos de la interface
+    const inputEmail = document.querySelector('#email');
+    const inputAsunto = document.querySelector('#asunto');
+    const inputMensaje = document.querySelector('#mensaje');
+});
