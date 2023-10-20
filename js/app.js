43344a928b2c1bf887d5d07dc9f9500e29d120e4
LuisMendozaSanchez <tavo.tpc@gmail.com>
2023-10-19 22:31:52 -0600

    Actualizacion de funcionalidades app.js

--- js/app.js
+++ js/app.js
@@ -3,4 +3,51 @@ document.addEventListener('DOMContentLoaded', function(){
     const inputEmail = document.querySelector('#email');
     const inputAsunto = document.querySelector('#asunto');
     const inputMensaje = document.querySelector('#mensaje');
-});
+    const formulario = document.querySelector('#formulario');
+
+    // Asignar eventos
+    inputEmail.addEventListener('blur', validar);
+    inputAsunto.addEventListener('blur', validar);
+    inputMensaje.addEventListener('blur', validar);
+
+    function validar(e){
+        if(e.target.value.trim() === ''){
+            mostrarAlerta(`El campo ${e.target.id} es obligatorio`, e.target.parentElement);
+            return;
+        }
+
+        if(e.target.id ==='email' && !validarEmail(e.target.value)){
+            mostrarAlerta('El email no es valido', e.target.parentElement);
+            return;
+        };
+
+        limpiarAlerta(e.target.parentElement);
+    }
+    
+    function mostrarAlerta(mensaje, referencia){
+       limpiarAlerta(referencia);
+
+        // Generar alerta en html
+        const error = document.createElement('p');
+        error.textContent = mensaje;
+        error.classList.add('bg-red-600', 'text-white', 'p-2', 'text-center');
+
+        // Inyectar el error al formulario
+        referencia.appendChild(error);
+    }
+    
+    function limpiarAlerta(referencia){
+        // Comprueba si ya existe una alerta
+        const alerta = referencia.querySelector('.bg-red-600');
+        if(alerta){
+            alerta.remove();
+        }
+    }
+
+    function validarEmail(email){
+        const regex =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+        const resultado = regex.test(email);
+        return resultado;
+    }
+
+}); 
