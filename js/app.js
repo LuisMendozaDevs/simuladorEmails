6a85f9cbda72276e35b976332b7bf2913b65dba7
LuisMendozaSanchez <tavo.tpc@gmail.com>
2023-10-20 11:40:19 -0600

    Se agregaron mas funcionalidades al app.js para validar los campos del formulario

--- js/app.js
+++ js/app.js
@@ -1,27 +1,56 @@
 document.addEventListener('DOMContentLoaded', function(){
+
+    const email = {
+        email: '',
+        asunto: '',
+        mensaje: ''
+    }
+
     // Seleccionar los elementos de la interface
     const inputEmail = document.querySelector('#email');
     const inputAsunto = document.querySelector('#asunto');
     const inputMensaje = document.querySelector('#mensaje');
     const formulario = document.querySelector('#formulario');
+    const btnSubmit = document.querySelector('#formulario button[type="submit"]');
+    const btnReset = document.querySelector('#formulario button[type="reset"]');
 
     // Asignar eventos
-    inputEmail.addEventListener('blur', validar);
-    inputAsunto.addEventListener('blur', validar);
-    inputMensaje.addEventListener('blur', validar);
+    inputEmail.addEventListener('input', validar);
+    inputAsunto.addEventListener('input', validar);
+    inputMensaje.addEventListener('input', validar);
+
+    btnReset.addEventListener('click', function(e){
+        e.preventDefault();
+        // Reiniciar el objeto
+        email.email = '';
+        email.asunto = '';
+        email.mensaje = '';
+        formulario.reset();
+        comprobarEmail();
+    });
 
     function validar(e){
         if(e.target.value.trim() === ''){
             mostrarAlerta(`El campo ${e.target.id} es obligatorio`, e.target.parentElement);
+            email[e.target.name] = '';
+            comprobarEmail();
             return;
         }
 
         if(e.target.id ==='email' && !validarEmail(e.target.value)){
             mostrarAlerta('El email no es valido', e.target.parentElement);
+            email[e.target.name] = '';
+            comprobarEmail();
             return;
         };
 
         limpiarAlerta(e.target.parentElement);
+
+        // Asignar los valores despues de haber pasado el proceso de validacion
+        email[e.target.name] = e.target.value.trim().toLowerCase();
+        // Comprobar el objeto de email
+        comprobarEmail();
+
     }
     
     function mostrarAlerta(mensaje, referencia){
@@ -50,4 +79,14 @@ document.addEventListener('DOMContentLoaded', function(){
         return resultado;
     }
 
+    function comprobarEmail(){
+        if(Object.values(email).includes('')){
+            btnSubmit.classList.add('opacity-50');
+            btnSubmit.disabled = true;
+            return;
+        }
+            btnSubmit.classList.remove('opacity-50');
+            btnSubmit.disabled = false;
+       
+    }
 }); 
