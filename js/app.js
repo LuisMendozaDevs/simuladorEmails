5a09eb99a6b50a73d6f0970e356141542691b504
LuisMendozaSanchez <tavo.tpc@gmail.com>
2023-10-21 15:27:25 -0600

    Version final del proyecto envio de emails

--- js/app.js
+++ js/app.js
@@ -13,21 +13,43 @@ document.addEventListener('DOMContentLoaded', function(){
     const formulario = document.querySelector('#formulario');
     const btnSubmit = document.querySelector('#formulario button[type="submit"]');
     const btnReset = document.querySelector('#formulario button[type="reset"]');
+    const spinner = document.querySelector('#spinner');
 
     // Asignar eventos
     inputEmail.addEventListener('input', validar);
     inputAsunto.addEventListener('input', validar);
     inputMensaje.addEventListener('input', validar);
 
+    formulario.addEventListener('submit', enviarEmail);
+
     btnReset.addEventListener('click', function(e){
         e.preventDefault();
-        // Reiniciar el objeto
-        email.email = '';
-        email.asunto = '';
-        email.mensaje = '';
-        formulario.reset();
-        comprobarEmail();
+        resetearFormulario();
     });
+
+    function enviarEmail(e){
+        e.preventDefault();
+
+        spinner.classList.add('flex');
+        spinner.classList.remove('hidden');
+
+        setTimeout(() => {
+            spinner.classList.remove('flex');
+            spinner.classList.add('hidden');
+            resetearFormulario();
+            // Crear una alerta
+            const alertaExito = document.createElement('p');
+            alertaExito.classList.add('bg-green-500', 'text-white', 'p-2', 'text-center', 'rounded-lg', 'mt-10', 'font-bold', 'text-sm', 'uppercase');
+            alertaExito.textContent = 'Mensaje enviado corectamente';
+            formulario.appendChild(alertaExito);
+
+            setTimeout(() => {
+                alertaExito.remove();
+            }, 3000);
+
+        }, 3000);
+        
+    }
 
     function validar(e){
         if(e.target.value.trim() === ''){
@@ -88,5 +110,14 @@ document.addEventListener('DOMContentLoaded', function(){
             btnSubmit.classList.remove('opacity-50');
             btnSubmit.disabled = false;
        
+    }
+
+    function resetearFormulario(){
+        // Reiniciar el objeto
+        email.email = '';
+        email.asunto = '';
+        email.mensaje = '';
+        formulario.reset();
+        comprobarEmail();
     }
 }); 
